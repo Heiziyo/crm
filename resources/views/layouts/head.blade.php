ffc9479cfa429f43d1e0c14e408b45799e3f3fba
heizi <960553031@qq.com>
2016-11-16 00:10:27 +0800

    up tam

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -4,7 +4,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>管理平台</title>
-    <link rel="stylesheet" href="/static/css/bootstrap.css">
+    <link rel="stylesheet" href="/layui/css/layui.css">
+    <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/my.css">
 </head>
 
 <body>
--- resources/views/team/index.blade.php
+++ resources/views/team/index.blade.php
@@ -1,39 +1,60 @@
 @extends('layouts.head')
-<link rel="stylesheet" href="/static/css/mycss.css">
 @section('content')
-    <div class="container-fluid  " id="mycontainer" >
-        <div class="nav navbar-default">
-            <ul class="nav navbar-nav">
-                <li><a href="{{ url('team') }}">项目组</a></li>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
-                <li><a href=""></a></li>
-            </ul>
-
+    <div class="container-fluid mycontainer">
+        <div class="header">
+            <div class="myMenu pull-left">
+                <ul>
+                    <li><a href="">消息</a></li>
+                    <li class="activetag"><a href="{{ url('team') }}">项目组</a></li>
+                </ul>
+            </div>
+            <div class="myD pull-right">
+                ddd
+            </div>
         </div>
-        <div class="container-fluid" id="mycontainer">
+
+        <div class="clearfix"></div>
+        <div class="col-md-2">
             <div class="row">
-                <div class="col-md-2">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h3 class="panel-title">我的项目组<i class="glyphicon glyphicon-plus pull-right " id="addGroup"></i></h3>
-                        </div>
-                        <div class="panel-body">
-                            dd
-                        </div>
+                <div class="panel panel-danger" style="height: 800px;">
+                   <div class="panel-heading">
+                        <h3 class="panel-title">
+                            我的项目组 <i class="glyphicon glyphicon-plus pull-right " style="cursor:pointer"></i>
+                        </h3>
+                    </div>
+                    <div class="panel-body">
+                        面板内容
                     </div>
                 </div>
-                <div class="col-md-10">
-                    d
+            </div>
+        </div>
+        <div class="col-md-10">
+            <div class="panel panel-danger" style="height: 800px;">
+                <div class="panel-heading">
+                    <h3 class="panel-title">
+                        我的项目组
+                    </h3>
+                </div>
+                <div class="panel-body">
+                    面板内容
                 </div>
             </div>
         </div>
     </div>
+    <script src="/layui/layui.js" ></script>
+    <script>
 
+        //一般直接写在一个js文件中
+        layui.use(['layer', 'form'], function(){
+            var layer = layui.layer
+                    ,$ = layui.jquery
+                    ,form = layui.form();
+            $("#addGroup").on('click',function () {
+                layer.msg(22)
+            })
 
-
-    <script src="/js/require.js" data-main="/js/main"></script>
+        });
+    </script>
 @endsection
 
 
