488a4c466089b49fc16eddd3b07c22edc0bc68c4
heizi <960553031@qq.com>
2016-11-19 02:07:05 +0800

    ''

--- resources/views/project/adddesign.blade.php
+++ resources/views/project/adddesign.blade.php
@@ -3,8 +3,23 @@
 
 @section('content')
     <divc class="container-fluid">
-        <div class="row" style="">
+        <div class="row" style="margin: 0;">
+            <div class="col-md-3">
+                <div class="panel">
+                    <div class="page-header">
+                        <h3 class="panel-title panel-danger">
+                            黑子
+                        </h3>
+                    </div>
+                    <div class="panel-body">
+                        <div class="cover"><img src="/public/images/cover.png" alt=""></div>
+                        <button class="btn-default">封面编辑(规格)</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-9">
 
+            </div>
         </div>
     </divc>
 @endsection
