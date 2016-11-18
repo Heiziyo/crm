c40deedf5208df26bb316b6f00a2bee88cee0a0e
heizi <960553031@qq.com>
2016-11-19 02:41:25 +0800

    ''

--- app/Http/Controllers/Project/ProjectController.php
+++ app/Http/Controllers/Project/ProjectController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers\Project;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use OSS\OssClient;
+use OSS\Core\OssException;
 class ProjectController extends Controller
 {
     public function index(){
@@ -21,7 +22,12 @@ class ProjectController extends Controller
      */
     function addDesign(){
 
-
+        $ossClient  = new OssClient();
+        try {
+            $ossClient = new OssClient(env('ALIOSS_ACCESSKEYID', ''), env('ALIOSS_ACCESSKEYSECRET', ''), env('ALIOSS_ENDPOINT', ''));
+        } catch (OssException $e) {
+            print $e->getMessage();
+        }
         return view('project.adddesign');
 
     }
