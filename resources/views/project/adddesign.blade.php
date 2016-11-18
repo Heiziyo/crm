13d0bef7eb05d9679225ac7a790e12ad680ec48e
heizi <960553031@qq.com>
2016-11-19 02:08:13 +0800

    ''

--- resources/views/project/adddesign.blade.php
+++ resources/views/project/adddesign.blade.php
@@ -12,7 +12,7 @@
                         </h3>
                     </div>
                     <div class="panel-body">
-                        <div class="cover"><img src="/public/images/cover.png" alt=""></div>
+                        <div class="cover"><img src="/images/cover.png" alt=""></div>
                         <button class="btn-default">封面编辑(规格)</button>
                     </div>
                 </div>
