4a63dae329236dffc8f5259314c162cbb4554242
heizi <960553031@qq.com>
2016-12-13 23:03:31 +0800

    ''

--- app/Http/Controllers/Manage/ManageController.php
+++ app/Http/Controllers/Manage/ManageController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers\Manage;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
 
 class ManageController extends Controller
 {
