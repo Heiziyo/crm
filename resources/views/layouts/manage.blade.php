24f088ded88f02338ff1567aa8ab8ee50c84f482
heizi <960553031@qq.com>
2016-12-13 20:20:14 +0800

    'managestyle'

--- resources/views/layouts/manage.blade.php
+++ resources/views/layouts/manage.blade.php
@@ -1005,20 +1005,7 @@
                 <div class="row">
                     <div class="col-xs-12">
                         <!-- PAGE CONTENT BEGINS -->
-                        <div class="alert alert-block alert-success">
-                            <button type="button" class="close" data-dismiss="alert">
-                                <i class="ace-icon fa fa-times"></i>
-                            </button>
-
-                            <i class="ace-icon fa fa-check green"></i>
-
-                            Welcome to
-                            <strong class="green">
-                                Ace
-                                <small>(v1.3.1)</small>
-                            </strong>,
-                            the lightweight, feature-rich and easy to use admin template.
-                        </div>
+                        @yield('content')
 
                         <div class="row">
                             <div class="space-6"></div>
