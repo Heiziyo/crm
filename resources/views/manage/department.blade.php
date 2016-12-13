989bba127863b936533ac42855727f42b0dc64f2
heizi <960553031@qq.com>
2016-12-13 21:49:09 +0800

    ''

--- resources/views/manage/department.blade.php
+++ resources/views/manage/department.blade.php
@@ -42,17 +42,17 @@
     <div class="row">
         <div class="col-xs-12">
             <!-- PAGE CONTENT BEGINS -->
-            <div class="well well-sm">
-                <button type="button" class="close line-height-0" data-dismiss="alert">
-                    <i class="ace-icon fa fa-times smaller-75"></i>
-                </button>
+            {{--<div class="well well-sm">--}}
+                {{--<button type="button" class="close line-height-0" data-dismiss="alert">--}}
+                    {{--<i class="ace-icon fa fa-times smaller-75"></i>--}}
+                {{--</button>--}}
 
-                You can have a custom jqGrid download here:
-                <a href="http://www.trirand.com/blog/?page_id=6" target="_blank">
-                    http://www.trirand.com/blog/?page_id=6
-                    <i class="fa fa-external-link bigger-110"></i>
-                </a>
-            </div>
+                {{--You can have a custom jqGrid download here:--}}
+                {{--<a href="http://www.trirand.com/blog/?page_id=6" target="_blank">--}}
+                    {{--http://www.trirand.com/blog/?page_id=6--}}
+                    {{--<i class="fa fa-external-link bigger-110"></i>--}}
+                {{--</a>--}}
+            {{--</div>--}}
 
             <table id="grid-table"></table>
 
@@ -227,9 +227,9 @@
                 },
 
                 editurl: "/dummy.html",//nothing is saved
-                caption: "jqGrid with inline editing"
+                caption: "部门列表"
 
-                //,autowidth: true,
+                ,autowidth: true,
 
 
                 /**
