02c9b0833d913d62c9489cc85c5f7c6bea4f6429
heizi <960553031@qq.com>
2016-12-13 23:02:12 +0800

    ''

--- resources/views/manage/department.blade.php
+++ resources/views/manage/department.blade.php
@@ -172,7 +172,6 @@
                     {name:'status',index:"status"},
                     {name:'start_time',index:"start_time"},
                     {name:'num',index:"num"},
-                    {name:'_token',index:'_token',editable: true,edittype:"hidden", editoptions: {value:"{{ csrf_token() }}"}}
 
                 ],
                 viewrecords : true,
