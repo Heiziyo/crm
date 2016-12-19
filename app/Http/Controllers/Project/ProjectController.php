c755f88e9c7dc743a88dfb146767586dda30060b
Heizi <960553031@qq.com>
2016-12-19 12:14:26 +0800

    ''

--- app/Http/Controllers/Project/ProjectController.php
+++ app/Http/Controllers/Project/ProjectController.php
@@ -203,7 +203,7 @@ class ProjectController extends Controller
     public function projectUpImg(Request $request)
     {
         if ($request->isMethod('post')){
-            $uploads_dir = "/myfile";
+            $uploads_dir = "myfile";
 
             $tmp_name = $_FILES["uu"]["tmp_name"];
             // basename() may prevent filesystem traversal attacks;
